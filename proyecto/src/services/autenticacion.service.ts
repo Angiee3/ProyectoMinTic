177340ef139fc617003127068d0a0cc17b40c128
Angiee3 <angienataliapins@gmail.com>
2022-11-25 17:48:57 -0500

    Envío correos y encriptación de contraseña

--- proyecto/src/services/autenticacion.service.ts
+++ proyecto/src/services/autenticacion.service.ts
@@ -1,10 +1,21 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import generatePassword from 'password-generator';
+const generador = require("password-generator");
+const criptoJS = require("crypto-js");
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
   constructor(/* Add @inject to inject parameters */) {}
 
-  /*
-   * Add service methods here
-   */
+  GenerarClave(){
+    let clave = generatePassword(8, false);
+    return clave;
+  }
+
+  CifrarClave(clave : string){
+    let claveCifrada = criptoJS.MD5(clave).toString();
+    return claveCifrada;
+  }
+
+
 }
